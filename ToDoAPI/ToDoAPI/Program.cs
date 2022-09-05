49c5838a8bc948b3bc6d6fc355f7a12d3daa5630
nickguerra <nickg@maliciousgames.com>
2022-09-05 06:48:31 -0500

    Added database

--- ToDoAPI/ToDoAPI/Program.cs
+++ ToDoAPI/ToDoAPI/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
