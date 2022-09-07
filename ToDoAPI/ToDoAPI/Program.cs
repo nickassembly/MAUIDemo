c7b39b2178424e28b25fbfe674eae127ca1f2be4
nickguerra <nickg@maliciousgames.com>
2022-09-07 06:27:10 -0500

    Added create endpoint

--- ToDoAPI/ToDoAPI/Program.cs
+++ ToDoAPI/ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,15 @@ app.MapGet("api/todo", async (AppDbContext context) =>
     var items = await context.ToDos.ToListAsync();
 
     return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
 });
 
 app.Run();
