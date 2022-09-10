ca0776aa2d399bf4690785b583ab4d5c95e13ef8
nickguerra <nickg@maliciousgames.com>
2022-09-10 06:32:51 -0500

    Added model and client project

---
+++ ToDoMauiClient/ToDoMauiClient/DataServices/IRestDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoMauiClient.Models;
+
+namespace ToDoMauiClient.DataServices
+{
+    public interface IRestDataService
+    {
+        Task<List<ToDo>> GetAllToDosAsync();
+        Task AddToDoAsync(ToDo toDo);
+        Task UpdateToDoAsync(ToDo toDo);
+        Task DeleteToDoAsync(int id);
+        
+    }
+}
